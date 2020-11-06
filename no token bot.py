c7210025dc5c6a12fc8e6931c195a42938c39aa1
reep1 <73842515+reep1@users.noreply.github.com>
2020-11-06 19:20:10 +0000

    Update no token bot.py

--- no token bot.py
+++ no token bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '?')
 
-TOKEN = "INSERT TOKEN HERE"
+TOKEN = "NzY0ODcwMTI0NjUzMTE3NDYw.X4Mi0A.w5l5wKxbmcO1KMU2kwn2YPOZY1E"
 
 @client.event
 async def on_ready():
@@ -14,8 +14,8 @@ async def ping(ctx):
 
    await ctx.send(':ping_pong: Pong!')
 
-   @client.command()
-   async def say(ctx, message):
+@client.command()
+async def say(ctx, message):
        
    await ctx.send(f'{ctx.author.name} said {message}')
 
