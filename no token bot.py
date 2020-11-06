188322d3ad2c7d7126a8e8577fba9fed08ab4f78
hrdlol <hrdlol@pm.me>
2020-11-06 20:46:01 +0000

    Added moderation commands (WIP)
    Added ban
    Added kick
    Added unban

--- no token bot.py
+++ no token bot.py
@@ -24,4 +24,29 @@ async def say(ctx, message):
        
    await ctx.send(f'{ctx.author.name} said {message}')
 
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason = None):
+
+   await member.kick(reason = reason)
+   await ctx.send(f'Successfully kicked {member.mention}')
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason = None):
+
+   await member.ban(reason = reason)
+   await ctx.send(f'Successfully banned {member.mention}')
+
+@client.command()
+async def unban(ctx, *, member):
+   _busers = await ctx.guild.bans()
+   _bname, member_discriminator = member.split('#')
+
+   for ban_entry in _busers:
+      user = ban_entry.user
+
+      if (user.name, user.discriminator) == (_bname, member_discriminator):
+         await ctx.guild.unban(user)
+         await ctx.send(f'Successfully unbanned {user.name}')
+         return
+
 client.run(c['token'])
